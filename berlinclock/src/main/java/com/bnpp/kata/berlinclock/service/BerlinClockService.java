e91f1605d42c8557d00b2d104ee638726d0abfab
2024-DEV3-007 <2024-DEV3-007@gmail.com>
2024-10-12 00:20:19 +0530

    feat : First Two Lamps in Top Five Hour Row is RED for hour between ten and fourteen

--- berlinclock/src/main/java/com/bnpp/kata/berlinclock/service/BerlinClockService.java
+++ berlinclock/src/main/java/com/bnpp/kata/berlinclock/service/BerlinClockService.java
@@ -24,7 +24,9 @@ public class BerlinClockService {
         int hours = Integer.parseInt(time.getHours());
         String hourLamp;
 
-        if (hours >= 5 && hours <= 9)
+        if (hours >= 10 && hours <= 14)
+            hourLamp = "RROO";
+        else if (hours >= 5 && hours <= 9)
             hourLamp = "ROOO";
         else
             hourLamp = "OOOO";
