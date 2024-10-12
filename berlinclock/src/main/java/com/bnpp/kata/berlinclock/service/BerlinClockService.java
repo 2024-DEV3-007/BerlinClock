480e5dcef51ff08811402ef30f7a1cf8b7eda4ec
2024-DEV3-007 <2024-DEV3-007@gmail.com>
2024-10-12 17:07:43 +0530

    feat : All Lamp in One Minute Row should be OFF when minute is divisible by 5

--- berlinclock/src/main/java/com/bnpp/kata/berlinclock/service/BerlinClockService.java
+++ berlinclock/src/main/java/com/bnpp/kata/berlinclock/service/BerlinClockService.java
@@ -17,12 +17,17 @@ public class BerlinClockService {
         String hourLamp = getHoursLamp(time);
         String oneHourLamp = getOneHourLamp(time);
         String fiveMinuteLamp = getMinuteLamp(time);
+        String oneMinuteLamp = getOneMinuteLamp(time);
 
-        DetailedBerlinTime detailedBerlinTime = createDetailedBerlinTime(secondsLamp,hourLamp,oneHourLamp,fiveMinuteLamp);
+        DetailedBerlinTime detailedBerlinTime = createDetailedBerlinTime(secondsLamp,hourLamp,oneHourLamp,fiveMinuteLamp,oneMinuteLamp);
 
         return BerlinClockResponse.builder()
                 .detailedBerlinTime(detailedBerlinTime)
                 .build();
+    }
+
+    private String getOneMinuteLamp(TimeComponent time) {
+        return "OOOO";
     }
 
     private String getMinuteLamp(TimeComponent time) {
@@ -35,13 +40,14 @@ public class BerlinClockService {
         return mintLamps.replace("YYY", "YYR");
     }
 
-    private DetailedBerlinTime createDetailedBerlinTime(String secondsLamp, String hourLamp, String oneHourLamp, String fiveMinuteLamp) {
+    private DetailedBerlinTime createDetailedBerlinTime(String secondsLamp, String hourLamp, String oneHourLamp, String fiveMinuteLamp, String oneMinuteLamp) {
 
         return DetailedBerlinTime.builder()
                 .secondsLamp(secondsLamp)
                 .topFiveHourLamps(hourLamp)
                 .bottomOneHourLamps(oneHourLamp)
                 .topFiveMinuteLamps(fiveMinuteLamp)
+                .bottomOneMinuteLamps(oneMinuteLamp)
                 .build();
     }
 
