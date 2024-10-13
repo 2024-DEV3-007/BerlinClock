f7cc46bce587029104bdfb8f884dd37f67da8dff
2024-DEV3-007 <2024-DEV3-007@gmail.com>
2024-10-13 16:31:05 +0530

    feat : Check whether the input second is greater than zero

--- berlinclock/src/test/java/com/bnpp/kata/berlinclock/validation/TimeValidatorTest.java
+++ berlinclock/src/test/java/com/bnpp/kata/berlinclock/validation/TimeValidatorTest.java
@@ -88,4 +88,13 @@ public class TimeValidatorTest {
 
         assertThrows(TimeFormatException.class, () -> timeValidator.validateTimeValues(timeComponent));
     }
+
+    @Test
+    @DisplayName("Throw Time Format Exception : if the input second is less than 0")
+    public void convertToBerlinTime_passSecondLessThanZero_shouldThrowTimeFormatException() {
+
+        TimeComponent timeComponent = TimeComponent.builder().hours(ZERO).minutes(ZERO).seconds(MINUS_ONE).build();
+
+        assertThrows(TimeFormatException.class, () -> timeValidator.validateTimeValues(timeComponent));
+    }
 }
