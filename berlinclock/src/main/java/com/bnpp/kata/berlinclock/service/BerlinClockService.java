446c401bea0ac07279612d5ac8c081a1e39536af
2024-DEV3-007 <2024-DEV3-007@gmail.com>
2024-10-13 15:35:00 +0530

    feat : Check whether the input hour is null or empty

--- berlinclock/src/main/java/com/bnpp/kata/berlinclock/service/BerlinClockService.java
+++ berlinclock/src/main/java/com/bnpp/kata/berlinclock/service/BerlinClockService.java
@@ -1,5 +1,6 @@
 package com.bnpp.kata.berlinclock.service;
 
+import com.bnpp.kata.berlinclock.exception.TimeFormatException;
 import com.bnpp.kata.berlinclock.model.BerlinClockResponse;
 import com.bnpp.kata.berlinclock.model.DetailedBerlinTime;
 import com.bnpp.kata.berlinclock.model.TimeComponent;
@@ -17,11 +18,18 @@ public class BerlinClockService {
 	
     public BerlinClockResponse convertToBerlinTime(TimeComponent time) {
 
+        validateTimeValues(time);
         Map<String, String> lamps = calculateLamps(time);
 
         return BerlinClockResponse.builder()
                 .detailedBerlinTime(createDetailedBerlinTime(lamps))
                 .build();
+    }
+
+    private void validateTimeValues(TimeComponent time) {
+        if (time.getHours() == null || time.getHours().isEmpty()) {
+            throw new TimeFormatException(TIME_IS_EMPTY_ERROR);
+        }
     }
 
     private Map<String, String> calculateLamps(TimeComponent time) {
